644122ea8549e4a4c09dcf40e0e87c710abb9b04
Yongwei Hu <yongweihu@users.noreply.github.com>
2018-05-13 21:36:08 +0800

    解决 css 文件的文件名中包含空格时无法加载文件内容的问题

--- Core/Source/DTHTMLAttributedStringBuilder.m
+++ Core/Source/DTHTMLAttributedStringBuilder.m
@@ -677,7 +677,8 @@
 		
 		if ([type isEqualToString:@"text/css"])
 		{
-			NSURL *stylesheetURL = [NSURL URLWithString:href relativeToURL:_baseURL];
+			// href中可能包含有空格，所以我们需要处理一下
+			NSURL *stylesheetURL = [NSURL URLWithString:[href stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding] relativeToURL:_baseURL];
 			if ([stylesheetURL isFileURL])
 			{
 				NSString *stylesheetContent = [NSString stringWithContentsOfURL:stylesheetURL encoding:NSUTF8StringEncoding error:nil];
